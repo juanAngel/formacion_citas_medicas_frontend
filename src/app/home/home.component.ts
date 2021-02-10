589e1b24d5d99a4d7e3239e7db17b5a99de92869
Juan Angel <juanangel0x7c5@gmail.com>
2021-02-10 16:20:25 +0100

    implement home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DoctorService } from '../doctor.service';
+import { PatientService } from '../patient.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.sass']
 })
 export class HomeComponent implements OnInit {
+  username:string;
+  password:string;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, 
+    private router: Router,
+    private doctorService:DoctorService,
+    private patientService:PatientService) { }
 
   ngOnInit(): void {
+  }
+  async loginDoctor(){
+    if(this.username == null)
+      return
+    let usuarios = await  this.doctorService.findByUsuario(this.username);
+    if(usuarios.length == 1){
+      this.router.navigate(["/doctor",usuarios[0].id]);
+    }
+  }
+  async loginPatient(){
+    if(this.username == null)
+      return
+    let usuarios = await  this.patientService.findByUsuario(this.username);
+    if(usuarios.length == 1){
+      this.router.navigate(["/patient",usuarios[0].id]);
+    }
   }
 
 }
